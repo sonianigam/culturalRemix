abd6bcd4d5d7f952c87a10cb778153efbef38ede
Sonia Nigam <sonianigam@sbcglobal.net>
2017-02-27 16:02:06 -0600

    time indices of a segment

--- test_percussive_sep.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-import librosa
-import librosa.display
-
-y, sr = librosa.load('woman_got_culture.wav', offset=40, duration=10)
-D = librosa.stft(y)
-D_harmonic, D_percussive = librosa.decompose.hpss(D)
